152f16d97c48fcdb3f1984cf0d2be1c1c54ffff1
sashida <sashida@primagest.co.jp>
2020-09-14 08:02:05 +0900

    p.228-2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 
+let name = "Adam";
+let city = "London";
+
 export default class extends Component {
-  render = () => React.createElement("h4", { className: "bg-primary text-white text-center p-3" },
-                  "This is an HTML element");
+
+  message = () => `Hello ${name} from ${city}`;
+
+  render = () =>
+    <h4 className="bg-primary text-white text-center p-3">
+      { this.message() }
+    </h4>
 };
 
