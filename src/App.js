fe2dab6e45bd89d308a3db96a603ac161c244b6b
sashida <sashida@primagest.co.jp>
2020-09-14 08:00:30 +0900

    p.228

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 
 export default class extends Component {
-  render = () =>
-    <h4 className="bg-primary text-white text-center p-3">
-      This is an HTML Element
-    </h4>
+  render = () => React.createElement("h4", { className: "bg-primary text-white text-center p-3" },
+                  "This is an HTML element");
 };
 
