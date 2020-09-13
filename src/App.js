e060b6102ab31ac9a6feb7c8a0d79a5719005ad3
sashida <sashida@primagest.co.jp>
2020-09-14 08:14:42 +0900

    p.240-1

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import reactLogo from "./logo.svg";
 let name = "Adam";
 let city = "London";
 
+// eslint-disable-next-line no-unused-vars
 let error = "not a valid statement";
 
 export default class extends Component {
